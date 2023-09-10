0c58e35d536d16e80db9c560481cf84960c5de05
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-09-10 21:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+let arr=[
+	"The Virupaksha Temple",
+	"Victoria Memorial",
+	"Tajmahal",
+];
+let articleLessArray=[];
 
+let mp={};
+
+for(let i=0;i<arr.length;i++){
+	let articleLessItem=arr[i].replace(regexp,"").trim();
+	articleLessArray.push(articleLessItem);
+	mp[articleLessItem]=arr[i];
+}
+articleLessArray.sort();
+
+let ans=[];
+for(let i of articleLessArray){
+	ans.push(mp[i]);
+}
+console.log(ans);
